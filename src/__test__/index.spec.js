bb4b1d9e39685a9eda7f010ad271d3d7ffbba9c2
Ondrej Bartas <ondrej@bartas.cz>
2017-04-12 23:25:27 +0200

    Add basic structure

---
+++ src/__test__/index.spec.js
@@ -0,0 +1,5 @@
+import index from '../index';
+
+it('should return function', () => {
+  expect(typeof index).toEqual('function');
+});
---
+++ src/index.js
@@ -0,0 +1,3 @@
+export default () => {
+  console.log('new js package'); // eslint-disable-line no-console
+};
